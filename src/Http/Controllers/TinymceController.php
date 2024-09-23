2dcb2bf1f9e46c34b0f9514b6c81af2c0ad83aae
kfteam <65326397+kfteam1991@users.noreply.github.com>
2024-09-24 05:48:21 +1000

    Update TinymceController.php
    增加删除图片方法

--- src/Http/Controllers/TinymceController.php
+++ src/Http/Controllers/TinymceController.php
@@ -22,6 +22,27 @@ class TinymceController
         return ['location' => $disk->url("{$dir}/$newName")];
     }
 
+    /**
+     * 删除图片
+     *
+     * @return void
+     */
+    public function delete(Request $request)
+    {
+        $imagePath = urldecode($request->input('image'));//路径解码
+      // 提取相对路径
+        $imagePath = str_replace(url('storage/'), '', $imagePath); 
+       // 使用 DIRECTORY_SEPARATOR 确保路径兼容性
+        $imagePath = 'public' . DIRECTORY_SEPARATOR . $imagePath;
+        // 删除图片逻辑
+        if (Storage::exists($imagePath)) {
+            $result = Storage::delete($imagePath);
+            return response()->json(['success' =>  $result, 'message' => 'Image deleted successfully.']);
+        }
+    
+        return response()->json(['success' => false, 'message' => 'Image not found.'], 404);
+    }
+
     protected function generateNewName(UploadedFile $file)
     {
         return uniqid(md5($file->getClientOriginalName())).'.'.$file->getClientOriginalExtension();
