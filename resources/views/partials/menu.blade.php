aab6081026f4a4998b013c0fa5810d8780c289a6
kfteam <65326397+kfteam1991@users.noreply.github.com>
2024-09-23 12:09:47 +0200

    Update menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -6,42 +6,39 @@
     $defaultIcon = config('admin.menu.default_icon', 'feather icon-circle');
 @endphp
 
-@if($builder->visible($item))
-    @if(empty($item['children']))
-        <li class="nav-item">
-            <a data-id="{{ $item['id'] ?? '' }}" @if(mb_strpos($item['uri'], '://') !== false) target="_blank" @endif
-               href="{{ $builder->getUrl($item['uri']) }}"
-               class="nav-link {!! $builder->isActive($item) ? 'active' : '' !!}">
-                {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
-                <p>
-                    {!! $builder->translate($item['title']) !!}
-                </p>
-            </a>
-        </li>
-    @else
-
-        <li class="{{ $horizontal ? 'dropdown' : 'has-treeview' }} {{ $depth > 0 ? 'dropdown-submenu' : '' }} nav-item {{ $builder->isActive($item) ? 'menu-open' : '' }}">
-            <a href="#"  data-id="{{ $item['id'] ?? '' }}"
-               class="nav-link {{ $builder->isActive($item) ? ($horizontal ? 'active' : '') : '' }}
+@if (empty($item['children']))
+    <li class="nav-item">
+        <a data-id="{{ $item['id'] ?? '' }}" @if (mb_strpos($item['uri'], '://') !== false) target="_blank" @endif
+            href="{{ $builder->getUrl($item['uri']) }}" class="nav-link {!! $builder->isActive($item) ? 'active' : '' !!}">
+            {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
+            <p>
+                {!! $builder->translate($item['title']) !!}
+            </p>
+        </a>
+    </li>
+@else
+    <li
+        class="{{ $horizontal ? 'dropdown' : 'has-treeview' }} {{ $depth > 0 ? 'dropdown-submenu' : '' }} nav-item {{ $builder->isActive($item) ? 'menu-open' : '' }}">
+        <a href="#" data-id="{{ $item['id'] ?? '' }}"
+            class="nav-link {{ $builder->isActive($item) ? ($horizontal ? 'active' : '') : '' }}
                     {{ $horizontal ? 'dropdown-toggle' : '' }}">
-                {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
-                <p>
-                    {!! $builder->translate($item['title']) !!}
+            {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
+            <p>
+                {!! $builder->translate($item['title']) !!}
 
-                    @if(! $horizontal)
-                        <i class="right fa fa-angle-left"></i>
-                    @endif
-                </p>
-            </a>
-            <ul class="nav {{ $horizontal ? 'dropdown-menu' : 'nav-treeview' }}">
-                @foreach($item['children'] as $item)
-                    @php
-                        $item['depth'] = $depth + 1;
-                    @endphp
+                @if (!$horizontal)
+                    <i class="right fa fa-angle-left"></i>
+                @endif
+            </p>
+        </a>
+        <ul class="nav {{ $horizontal ? 'dropdown-menu' : 'nav-treeview' }}">
+            @foreach ($item['children'] as $item)
+                @php
+                    $item['depth'] = $depth + 1;
+                @endphp
 
-                    @include('admin::partials.menu', ['item' => $item])
-                @endforeach
-            </ul>
-        </li>
-    @endif
+                @include('admin::partials.menu', ['item' => $item])
+            @endforeach
+        </ul>
+    </li>
 @endif
