10747714503b9b0673e39ab803250aa80e902ecc
kfteam <65326397+kfteam1991@users.noreply.github.com>
2024-10-12 18:38:44 +1000

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 
 {!! admin_section(Dcat\Admin\Admin::SECTION['NAVBAR_BEFORE']) !!}
 
-<nav class="header-navbar navbar-expand-lg navbar
+<nav class="navbar-expand-lg navbar
     navbar-with-menu {{ $configData['navbar_class'] }}
     {{ $configData['navbar_color'] }}
         navbar-light navbar-shadow " style="top: 0;">
