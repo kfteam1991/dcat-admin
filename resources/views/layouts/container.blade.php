4b9e5830e64fc42509f60448ba728e7c174680c5
kfteam <65326397+kfteam1991@users.noreply.github.com>
2024-11-02 22:10:27 +0100

    Update container.blade.php

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -4,6 +4,7 @@
 
 <script>
     var Dcat = CreateDcat({!! Dcat\Admin\Admin::jsVariables() !!});
+    var currentLoginUserId = {{ json_encode(Dcat\Admin\Admin::user()->id) }};
 </script>
 
 {!! admin_section(Dcat\Admin\Admin::SECTION['BODY_INNER_BEFORE']) !!}
