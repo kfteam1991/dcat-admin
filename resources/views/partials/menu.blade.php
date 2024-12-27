0b92d48f3b48a999521f5f74fb9a4b8d2c5e835d
kfteam <65326397+kfteam1991@users.noreply.github.com>
2024-12-27 20:44:20 +0100

    Update menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -10,7 +10,7 @@
     <li class="nav-item">
         <a data-id="{{ $item['id'] ?? '' }}" @if (mb_strpos($item['uri'], '://') !== false) target="_blank" @endif
             href="{{ $builder->getUrl($item['uri']) }}" class="nav-link {!! $builder->isActive($item) ? 'active' : '' !!}">
-            {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
+            {!! str_repeat('&nbsp;', $depth) !!}<i class="{{ $item['icon'] ?: $defaultIcon }}"></i>
             <p>
                 {!! $builder->translate($item['title']) !!}
             </p>
@@ -22,7 +22,7 @@
         <a href="#" data-id="{{ $item['id'] ?? '' }}"
             class="nav-link {{ $builder->isActive($item) ? ($horizontal ? 'active' : '') : '' }}
                     {{ $horizontal ? 'dropdown-toggle' : '' }}">
-            {!! str_repeat('&nbsp;', $depth) !!}<i class="fa fa-fw {{ $item['icon'] ?: $defaultIcon }}"></i>
+            {!! str_repeat('&nbsp;', $depth) !!}<i class="{{ $item['icon'] ?: $defaultIcon }}"></i>
             <p>
                 {!! $builder->translate($item['title']) !!}
 
